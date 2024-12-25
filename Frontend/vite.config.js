df6b1e8e1672ae946cab49a53986becd129c9ba6
Amit-Makhloga <amitmakhloga551@gmail.com>
2024-12-25 11:26:46 +0530

    Clear git cache

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,12 +9,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/BookStore/', // Set this to match the "homepage" field in package.json
+//   build: {
+//     outDir: 'build', // Ensures the build output goes to the "build" directory
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/BookStore/', // Set this to match the "homepage" field in package.json
-  build: {
-    outDir: 'build', // Ensures the build output goes to the "build" directory
-  },
+  base: '/BookStore/', // Ensure this is correct
+  plugins: [react()]
 });
+
 

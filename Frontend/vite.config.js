1ec842cb11b815a8987aa791f44cb45bf039ed29
Amit-Makhloga <amitmakhloga551@gmail.com>
2024-12-27 09:54:36 +0530

    adding basename

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,18 +9,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/BookStore/', // Set this to match the "homepage" field in package.json
-//   build: {
-//     outDir: 'build', // Ensures the build output goes to the "build" directory
-//   },
-// });
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/BookStore/', // Ensure this is correct
-  plugins: [react()]
+  plugins: [react()],
+  base: '/BookStore/', // Set this to match the "homepage" field in package.json
+  build: {
+    outDir: 'build', // Ensures the build output goes to the "build" directory
+  },
 });
-
 

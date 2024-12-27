5f9fd45d3d9a4f96ce2038c17977ff35e192978c
Amit-Makhloga <amitmakhloga551@gmail.com>
2024-12-27 09:59:31 +0530

    adding basename

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/BookStore/">
+  <BrowserRouter basename="/BookStore">
     <AuthProvider>
       <div className="dark:bg-slate-900 dark:text-white">
         <App />
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BookStore/', // Set this to match the "homepage" field in package.json
+  base: '/BookStore', // Set this to match the "homepage" field in package.json
   build: {
     outDir: 'build', // Ensures the build output goes to the "build" directory
   },
